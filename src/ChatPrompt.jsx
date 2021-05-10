c044f7847ad775e9e47c5309e9f0fee81f3994b4
Alec Kienzle <aleckienzle@Alecs-MBP-2.lan1>
2021-05-10 12:57:16 -0700

    added boxes

--- src/ChatPrompt.jsx
+++ src/ChatPrompt.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+const pastMessages = {
+	width: 300,
+	border: '10px solid #5F9EA0',
+	padding: 100,
+	margin: 'auto'
+}
+const currentMessage = {
+	width: 300,
+	paddingTop: 25,
+	paddingBottom: 25,
+	paddingRight: 100,
+	paddingLeft: 100,
+	borderBottom: '10px solid #5F9EA0',
+	borderLeft: '10px solid #5F9EA0',
+	borderRight: '10px solid #5F9EA0',
+	margin: 'auto'
+}
 
 export const Prompt = () => {
 	return (
-		<div>Chat Prompt</div>
+		<body>
+			<h1 style={{textAlign: 'center'}}>Chat App</h1>	
+			<div style={pastMessages}>
+				<p style={{textAlign: 'center'}}>This is where previous messages will be seen.</p>
+			</div>
+			<div style={currentMessage}>
+				<p style={{textAlign: 'center'}}>Prompt</p>
+			</div>	
+		</body>	
 	);
 };
