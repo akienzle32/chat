cf65dccbeff53b20b711d68bc014dd019fcf4522
Alec Kienzle <aleckienzle@Alecs-MBP-2.lan1>
2021-05-10 13:31:54 -0700

    made boxes scrollable and of a fixed height

--- src/ChatPrompt.jsx
+++ src/ChatPrompt.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 const pastMessages = {
+	height: 100,
+	overflowY: 'scroll',
 	width: 300,
 	border: '10px solid #5F9EA0',
-	padding: 100,
+	padding: 75,
 	margin: 'auto'
 }
 const currentMessage = {
+	height: 50,
+	overflowY: 'scroll',
 	width: 300,
 	paddingTop: 25,
 	paddingBottom: 25,
-	paddingRight: 100,
-	paddingLeft: 100,
+	paddingRight: 75,
+	paddingLeft: 75,
 	borderBottom: '10px solid #5F9EA0',
 	borderLeft: '10px solid #5F9EA0',
 	borderRight: '10px solid #5F9EA0',
@@ -22,10 +26,11 @@ export const Prompt = () => {
 		<body>
 			<h1 style={{textAlign: 'center'}}>Chat App</h1>	
 			<div style={pastMessages}>
-				<p style={{textAlign: 'center'}}>This is where previous messages will be seen.</p>
+				<p style={{textAlign: 'left'}}>This is where previous messages will be seen. I'm going to type
+				more words in order to see if the scroll function is working properly. It does indeed appear to work.</p>
 			</div>
 			<div style={currentMessage}>
-				<p style={{textAlign: 'center'}}>Prompt</p>
+				<p style={{textAlign: 'left'}}>Prompt</p>
 			</div>	
 		</body>	
 	);
