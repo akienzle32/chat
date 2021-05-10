554cf66cc9f9e852e9af42d8d5e049d1ba49445c
Alec Kienzle <aleckienzle@Alecs-MacBook-Pro-2.local>
2021-05-10 16:18:37 -0700

    added an additional box for typing and adjusted some spacing

--- src/ChatPrompt.jsx
+++ src/ChatPrompt.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
-const pastMessages = {
-	height: 100,
+const upperBox = {
+	height: 200,
 	overflowY: 'scroll',
-	width: 300,
-	border: '10px solid #5F9EA0',
-	padding: 75,
+	width: 400,
+	border: '7px solid #5F9EA0',
+	padding: 50,
+	margin: 'auto'
+}
+const lowerBox = {
+	height: 50,
+	width: 400,
+	paddingTop: 25,
+	paddingBottom: 25,
+	paddingRight: 50,
+	paddingLeft: 50,
+	borderBottom: '7px solid #5F9EA0',
+	borderLeft: '7px solid #5F9EA0',
+	borderRight: '7px solid #5F9EA0',
 	margin: 'auto'
 }
 const currentMessage = {
-	height: 50,
-	overflowY: 'scroll',
-	width: 300,
-	paddingTop: 25,
-	paddingBottom: 25,
-	paddingRight: 75,
-	paddingLeft: 75,
-	borderBottom: '10px solid #5F9EA0',
-	borderLeft: '10px solid #5F9EA0',
-	borderRight: '10px solid #5F9EA0',
-	margin: 'auto'
+	height: 20,
+	width: 385,
+	overflowY: 'scroll', 
+	paddingLeft: 5, 
+	paddingRight: 5, 
+	paddingBottom: 10, 
+	paddingTop: 10, 
+	margin: 0, 
+	border: '2px solid black', 
+	borderRadius: 15
 }
 
 export const Prompt = () => {
 	return (
 		<body>
 			<h1 style={{textAlign: 'center'}}>Chat App</h1>	
-			<div style={pastMessages}>
-				<p style={{textAlign: 'left'}}>This is where previous messages will be seen. I'm going to type
-				more words in order to see if the scroll function is working properly. It does indeed appear to work.</p>
+			<div style={upperBox}>
+				<p style={{textAlign: 'left'}}>This is where previous messages will appear.</p>
 			</div>
-			<div style={currentMessage}>
-				<p style={{textAlign: 'left'}}>Prompt</p>
+			<div style={lowerBox}>
+				<div style={currentMessage}>
+					This is where current messages will be typed...
+				</div>	
 			</div>	
 		</body>	
 	);
