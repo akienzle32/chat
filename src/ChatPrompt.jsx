99a66eb76d34cf10ed98aed8ea4c75d37bf79b88
Alec Kienzle <alec.kienzle@gmail.com>
2021-05-24 14:16:55 -0700

    put css elements in separate file

--- src/ChatPrompt.jsx
+++ src/ChatPrompt.jsx
@@ -1,68 +1,30 @@
 import React from 'react';
-
-const upperBox = {
-	backgroundColor: '#FDF5E6',
-	height: 200,
-	overflowY: 'scroll',
-	width: 400,
-	borderRadius: 25,
-	border: '7px solid #5F9EA0',
-	padding: 50,
-	margin: 'auto'
-}
-const lowerBox = {
-	backgroundColor: '#FDF5E6',
-	height: 50,
-	width: 400,
-	paddingTop: 25,
-	paddingBottom: 25,
-	paddingRight: 50,
-	paddingLeft: 50,
-	borderRadius: 25,
-	borderBottom: '7px solid #5F9EA0',
-	borderLeft: '7px solid #5F9EA0',
-	borderRight: '7px solid #5F9EA0',
-	margin: 'auto'
-}
-const currentMessage = {
-	backgroundColor: 'white',
-	height: 20,
-	width: 385,
-	overflowY: 'scroll', 
-	paddingLeft: 5, 
-	paddingRight: 5, 
-	paddingBottom: 10, 
-	paddingTop: 10, 
-	margin: 0, 
-	fontSize: 16
-}
+import './Chat.css';
 
 var i = 0;
-var messageLog = Array();
+var messageLog = [];
 
-function myInput(e){
-	if (e.key==='Enter'){
+function myInput(enter){
+	if (enter.key==='Enter'){
 		messageLog[i] = document.getElementById("chat-input").value;
- 		i++;
+		i++;
  		document.getElementById("chat-input").value = "";
 
    			var lastMessage = "";   
    			for (var j=0; j < messageLog.length; j++){
-     		lastMessage += messageLog[j] + "<br/><br/>";
+     			lastMessage += "<p>" + messageLog[j] + "</p>";
   			}
  			document.getElementById("chat-log").innerHTML = lastMessage;
 	}
-};	
+}
 
 export const Prompt = () => {
 	return (
 		<body>
 			<h1 style={{textAlign: 'center'}}>Chat App</h1>	
-			<div style={upperBox}>
-				<div id="chat-log"></div>
-			</div>
-			<div style={lowerBox}>
-				<input type="text" id="chat-input" style={currentMessage} onKeyUp={myInput} placeHolder="Text message..."></input>
+			<div id="chat-log"></div>
+			<div id="lower-box">
+				<input type="text" id="chat-input" onKeyUp={myInput} placeHolder="Text message..."></input>
 			</div>	
 		</body>	
 	);
