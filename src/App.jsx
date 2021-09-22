9afa1060447087e2681812632ac85739d4fcb609
Alec Kienzle <alec.kienzle@gmail.com>
2021-09-22 14:12:13 -0700

    ChatRoom successfully navigating back to Home after lifting state up to App and changing the route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,69 @@ import { Home } from './Home';
 import {
   BrowserRouter as Router,
   Switch,
-  Route } from "react-router-dom";
+  Route,
+  Link } from "react-router-dom";
 
 export class App extends React.Component {
+  constructor(props){
+  	super(props);
+  	// Test data for managing state. 
+  	this.state = {
+  		username: "alec",
+  		chats: [{id:1, name:"Django"}, {id:2, name:"React"}],
+  		participants:
+  			[{id:1, username:"alec", chat_id:1}, 
+  			{id:2, username:"matt", chat_id:1},
+  			{id:3, username:"alec", chat_id:2},
+  			{id:4, username:"carol", chat_id:2}],
+  		isClicked: false,
+  	}
+  }
 
+  addChat = (chatname, ptcpname) => {
+  	const chats = this.state.chats;
+  	const lastChat = chats[chats.length - 1];
+    const chatId = lastChat.id + 1;
+    const JSONchat = {id:chatId, name:chatname}
+  	const newChats = chats.concat(JSONchat);
+  	console.log(newChats);
+
+  	const myUsername = this.state.username;
+  	const ptcps = this.state.participants;
+  	const lastPtcp = ptcps[ptcps.length - 1];
+  	const ptcpId = lastPtcp.id + 1;
+  	const firstJsonPtcp = {id:ptcpId, username:myUsername, chat_id:chatId};
+  	const secondJsonPtcp = {id:ptcpId+1, username:ptcpname, chat_id:chatId};
+  	const tempPtcps = ptcps.concat(firstJsonPtcp);
+  	const newPtcps = tempPtcps.concat(secondJsonPtcp);
+  	console.log(newPtcps);
+  		this.setState({
+  			participants: newPtcps,
+  		})
+
+  	this.setState({
+  		chats: newChats,
+  	})
+  }
+
+  handleClick = () => {
+  	this.setState({isClicked: true});
+  }
 
   render() {
 	return(
 	  <Router>
+	  	<div className="nav-bar">
+  		  <Link className="link" to="/">Home</Link>
+	  	  <a className="link" id="logout" href="http://127.0.0.1:8000/accounts/logout">Log out</a>
+		</div>
 	 	<Switch>
+	 	  <Route path="/:name">
+  			<ChatRoom participants={this.state.participants} />;
+  		  </Route>
 		  <Route path="/">
-			  <Home />
+			<Home chats={this.state.chats} participants={this.state.participants} 
+				onSubmit={this.addChat} onClick={this.handleClick} />
 		  </Route>
 		</Switch>
 	  </Router>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,74 +1,25 @@
 import React from 'react';
 import { StartChat } from './StartChat';
 import { ChatRoom } from './ChatRoom';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
 export class Home extends React.Component {
-  constructor(props){
-  	super(props);
-  	// Test data for managing state. 
-  	this.state = {
-  		username: "alec",
-  		chats: [{id:1, name:"Django"}, {id:2, name:"React"}],
-  		participants:
-  			[{id:1, username:"alec", chat_id:1}, 
-  			{id:2, username:"matt", chat_id:1},
-  			{id:3, username:"alec", chat_id:2},
-  			{id:4, username:"carol", chat_id:2}],
-  		isClicked: false,
-  	}
-  }
 
   // Test function for updating the state of the user's chats. Eventually, this will be two POST requests. 
-  addChat = (chatname, ptcpname) => {
-  	const chats = this.state.chats;
-  	const lastChat = chats[chats.length - 1];
-    const chatId = lastChat.id + 1;
-    const JSONchat = {id:chatId, name:chatname}
-  	const newChats = chats.concat(JSONchat);
-  	console.log(newChats);
-
-  	const myUsername = this.state.username;
-  	const ptcps = this.state.participants;
-  	const lastPtcp = ptcps[ptcps.length - 1];
-  	const ptcpId = lastPtcp.id + 1;
-  	const firstJsonPtcp = {id:ptcpId, username:myUsername, chat_id:chatId};
-  	const secondJsonPtcp = {id:ptcpId+1, username:ptcpname, chat_id:chatId};
-  	const tempPtcps = ptcps.concat(firstJsonPtcp);
-  	const newPtcps = tempPtcps.concat(secondJsonPtcp);
-  	console.log(newPtcps);
-  		this.setState({
-  			participants: newPtcps,
-  		})
-
-  	this.setState({
-  		chats: newChats,
-  	})
-  }
 
   // Determines the conditional rendering of either the StartChat/ChatList components OR the ChatRoom component.
   // This gets passed down as a prop to StartChat and then eventually to ChatList. 
-  handleClick = () => {
-  	this.setState({isClicked: true});
-  }
+
 
   render() {
-  	let component;
 
   	// By default, the user's list of chats is displayed. If the user clicks on a chat, then ChatRoom is rendered. 
-  	if (this.state.isClicked)
-  		component = <ChatRoom participants={this.state.participants} />;
-  	else
-  		component = <StartChat chats={this.state.chats} participants={this.state.participants} 
-  	onSubmit={this.addChat} onClick={this.handleClick} />;
+  
 	return (
 	  <div>
-		<div className="nav-bar">
-  		  <Link className="link" to="/">Home</Link>
-	  	  <a className="link" id="logout" href="http://127.0.0.1:8000/accounts/logout">Log out</a>
-		</div>
 		<div>
-			{component}
+			<StartChat chats={this.props.chats} participants={this.props.participants} 
+  	onSubmit={this.props.onSubmit} onClick={this.props.onClick} />;
 		</div>
 	  </div>
 	);
