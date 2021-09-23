b1babd001dfa3034c8062374864c65e284bf82e6
Alec Kienzle <alec.kienzle@gmail.com>
2021-09-23 13:53:52 -0700

    changed add-ptcp id to a class

--- src/StartChat.jsx
+++ src/StartChat.jsx
@@ -3,6 +3,13 @@ import { ChatList } from './ChatList';
 
 export class StartChat extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      ptcpCounter: 1,
+    }
+  }
+
 
   // Function to pass text input up to App component, where the state of chats and participants 
   // is tracked. 
@@ -10,11 +17,22 @@ export class StartChat extends React.Component {
     event.preventDefault();
 
     const chatName = document.getElementById("chat-name").value;
-    const ptcpName = document.getElementById("ptc-name").value;
+    const ptcpArray = document.getElementsByClassName("ptcp-name");
+    const ptcpName = ptcpArray[0].value;
     this.props.onSubmit(chatName, ptcpName);
     document.getElementById("chat-form").reset();
   }
 
+  /*
+  addExtraParticipant(){
+    let maxParticipants = 5;
+    let ptcpCounter = this.state.ptcpCounter;
+    if (ptcpCounter < maxParticipants){
+      let extraInput = <input></input>
+    }
+
+  }
+  */
   render() {
   	return(
   	  <div>
@@ -25,8 +43,8 @@ export class StartChat extends React.Component {
         	  	  <div id="chat-form-container">
         	  		<form id="chat-form" onSubmit={this.onSubmit}>
     			  	    <input type="text" id="chat-name" name="chatname" placeholder="Enter a chatroom name..."></input>
-                  <input type="text" id="ptc-name" name="username" placeholder="Enter a username..."></input>
-                  <input type="text" id="add-ptc-button" value="+"></input>
+                  <input type="text" className="ptcp-name" name="username" placeholder="Enter a username..."></input>
+                  <input type="text" id="add-ptcp-button" value="+"></input>
            		    <input type="submit" id="submit-forms" className="submitButton"></input>
            			</form>
         	   	  </div>
