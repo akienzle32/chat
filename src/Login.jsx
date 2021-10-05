cb6ce07bd816fb89deb50910ededb608020673c6
Alec Kienzle <alec.kienzle@gmail.com>
2021-10-05 09:57:53 -0700

    added input boxes for username and password

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,6 +6,14 @@ export const Login = () => {
   return(
 	<div>
 	  <h1 className="chat-title">Login</h1>
+	  	<div style={{textAlign: 'center'}}>
+	  	  <form>
+	  	  	<p className="input-title">Username:  </p>
+	  	  	<input type="text" name="username"></input><br></br>
+	  	  	<p className="input-title">Password: </p>
+	  	  	<input type="password" name="password"></input>
+	  	  </form>
+	  	</div>
 	</div>
   );
 }
